f0a57fe60559ea61d2764f70b19e6a5baf8b98c5
zhongtaoaki <zhongtaodylan@163.com>
2019-11-09 22:23:19 +0900

    11月10日-demo

---
+++ src/demo1110/ZhangErGou.java
@@ -0,0 +1,14 @@
+package demo1110;
+
+public class ZhangErGou {
+
+	private String name = "次郎";
+	private int age = 2;
+	private String gender = "Girl";
+	private String sound = "miao";
+
+	public void makeSound() {
+		System.out.println(name + "が声している：" + sound);
+	}
+
+}
